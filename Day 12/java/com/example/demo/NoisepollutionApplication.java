1e9003750e943b70f0770119d764d2c6e65dfbe1
HARIVISHNU21 <110534442+HARIVISHNU21@users.noreply.github.com>
2023-10-05 14:35:04 +0530

    Add files via upload

---
+++ Day 12/java/com/example/demo/NoisepollutionApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoisepollutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoisepollutionApplication.class, args);
+	}
+
+}
