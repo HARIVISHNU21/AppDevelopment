7c723fae89750d5358339cbd561b111750fa4d8a
HARIVISHNU21 <110534442+HARIVISHNU21@users.noreply.github.com>
2023-10-11 11:45:37 +0530

    Add files via upload

---
+++ Day 15/noisesecurity/src/main/java/com/example/demo/NoisesecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoisesecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoisesecurityApplication.class, args);
+	}
+
+}
---
+++ Day 15/noisesecurity/src/test/java/com/project/fitra/NoisesecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.fitra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoisesecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
